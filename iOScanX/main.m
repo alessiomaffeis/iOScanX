26833a67eeeb7426fd02ca5f618745cff9618dfa
Alessio Maffeis <maffo88@gmail.com>
2013-03-06 13:49:45 +0100

    Initial Commit

---
+++ iOScanX/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  iOScanX
+//
+//  Created by Alessio Maffeis on 06/03/13.
+//  Copyright (c) 2013 Alessio Maffeis. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
